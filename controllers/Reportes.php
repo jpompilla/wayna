73deb295a5a949dc9d21b4e18b898fafd94a2258
Jose <jose.pompilla@gmail.com>
2026-07-09 17:37:13 -0500

    Flujo: Se ha incluido los costos fijos promedio

--- controllers/Reportes.php
+++ controllers/Reportes.php
@@ -39,7 +39,16 @@ class Reportes extends Controller
             $data[$row->mes]['egresos.pasarela'] = 0;
             $data[$row->mes]['egresos.impuestos'] = 0;
             $data[$row->mes]['egresos'] = 0;
+            $data[$row->mes]['margen.operativo'] = 0;
+            
+            $data[$row->mes]['fijo.administrativo'] = 0;
+            $data[$row->mes]['fijo.personal'] = 0;
+            $data[$row->mes]['fijo.cuentas '] = 0;
+            $data[$row->mes]['fijo.clientes'] = 0;
+            $data[$row->mes]['fijo'] = 0;
+
             $data[$row->mes]['utilidades'] = 0;
+
         }
         foreach ($rpta as $row) {
             $data[$row->mes]['ingresos.ejecutados'] += $row->ingresos; 
@@ -51,7 +60,19 @@ class Reportes extends Controller
             $data[$row->mes]['egresos.pasarela'] += $row->izipay; 
             $data[$row->mes]['egresos.impuestos'] += $row->impuestos; 
             $data[$row->mes]['egresos'] += $row->costo_operativo + $row->comision + $row->izipay + $row->impuestos;
+            $data[$row->mes]['margen.operativo'] += $row->ingresos - ($row->costo_operativo + $row->comision + $row->izipay + $row->impuestos);
+
+            $data[$row->mes]['fijo.administrativo'] = 560;
+            $data[$row->mes]['fijo.personal'] = 420;
+            $data[$row->mes]['fijo.cuentas'] = 300;
+            $data[$row->mes]['fijo.clientes'] = 300;
+            $data[$row->mes]['fijo'] = 1580;
+
             $data[$row->mes]['utilidades'] += $row->ingresos - ($row->costo_operativo + $row->comision + $row->izipay + $row->impuestos);
+        }
+
+        foreach ($data as $d => $mes) {
+            $data[$d]['utilidades'] -= $mes['fijo'];
         }
 
         ksort($data);
